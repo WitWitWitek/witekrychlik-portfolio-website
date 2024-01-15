52dd5f492d28dd78d6c6ea2efe0dc1435a097e39
Witold <witekrychlik@o2.pl>
2024-01-15 12:06:34 +0100

    feat: basic architecture and styling of homeview implemented

---
+++ web/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { Button } from '@witekrychlik/ui-components';
+import Image from 'next/image';
+
+export default function Navbar() {
+  return (
+    <nav className="fixed bg-slate-950 px-[100px] h-14 w-full flex items-center border-b border-white border-opacity-20">
+      <Link href="/" className="w-9 h-9 relative rounded-md overflow-hidden">
+        <Image src="/logo.svg" alt="Logo strony witekrychlik.com.pl" fill />
+      </Link>
+      <div className="flex flex-row gap-2 grow px-2">
+        <Button variant="secondary">Strona główna</Button>
+        <Button variant="secondary">Projekty</Button>
+        <Button variant="secondary">Technologie</Button>
+        <Button variant="secondary">Usługi</Button>
+      </div>
+      <div>
+        <Button variant="secondary">Kontakt</Button>
+      </div>
+    </nav>
+  );
+}
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,12 +1,51 @@
 import { Button } from '@witekrychlik/ui-components';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default async function Index() {
   return (
-    <div className="bg-slate-600 h-screen grid-flow-col-dense">
-      <button className="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white rounded cursor-pointer text-lg">
-        Click me
-      </button>
-      <Button variant="default">Button</Button>
-    </div>
+    <section className="bg-slate-950 h-screen px-[100px] py-14 flex flex-row">
+      <div className="h-full w-3/5 p-4 flex flex-col justify-center gap-2 text-white">
+        <div className="text-6xl flex flex-col gap-2">
+          <p>Cześć! 👋</p>
+          <p className="font-bold">
+            Jestem <span className="text-linear-gradient">Witold Rychlik</span>
+          </p>
+          <div className="flex items-center gap-5 text-[15px]">
+            <div className="grow h-[1px] linear-gradient" />
+            <span className="italic">Web developer</span>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p>
+            Jestem młodym pasjonatem web developmentu, gotowym przekształcić
+            Twoje pomysły w wirtualną rzeczywistość. Razem możemy stworzyć
+            projekt, który nie tylko zachwyci wizualnie, ale także dostarczy
+            niezapomniane wrażenia użytkownikom. Daj mi szansę pomóc Ci w
+            urzeczywistnieniu Twoich internetowych marzeń!
+          </p>
+          <div className="flex w-full gap-2">
+            <Button variant="default">Formularz kontaktowy</Button>
+            <Button variant="default">Technologie</Button>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p>Sprawdź:</p>
+          <div className="flex w-full gap-2">
+            <Link href="https://github.com/WitWitWitek">
+              <Button variant="destructive">Git</Button>
+            </Link>
+            <Link href="https://www.linkedin.com/in/witold-rychlik-86b579195/">
+              <Button variant="destructive">LinkedIn</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="h-full w-2/5 p-4 flex justify-center items-center">
+        <div className="relative w-36 h-36 rounded-md overflow-hidden">
+          <Image src="/logo.svg" alt="Logo strony witekrychlick.com.pl" fill />
+        </div>
+      </div>
+    </section>
   );
 }
