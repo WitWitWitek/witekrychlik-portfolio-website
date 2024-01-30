6ece615ad34603dc0c083a8022f63cf464c9f1ce
Witold <witekrychlik@o2.pl>
2024-01-30 11:36:48 +0100

    feat: sitemap.ts implemented

---
+++ web/app/sitemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from 'next';
+import { routes } from '../constants';
+import { client } from '../lib/sanity';
+
+const BASE_URL = 'https://witekrychlik.com.pl';
+
+async function getProjects(): Promise<{ slug: string }[]> {
+  const query = `*[_type == 'projects'] {
+      "slug": slug.current,
+    }`;
+
+  const projectsData = await client.fetch(query);
+  return projectsData;
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const routesXML: MetadataRoute.Sitemap = routes.map((route, i) => ({
+    url: `${BASE_URL}${route.target}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: i === 0 ? 1 : 0.75,
+  }));
+
+  const slugs = await getProjects();
+  const slugsXML: MetadataRoute.Sitemap = slugs.map((slug, i) => ({
+    url: `${BASE_URL}/projekty/${slug.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.5,
+  }));
+
+  return [
+    ...routesXML,
+    {
+      url: `${BASE_URL}/kontakt`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    ...slugsXML,
+  ];
+}
